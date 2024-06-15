7f7c58f9a27c20d9a50cd46c212a7e9a4658403b
1MatiasP <1matiaspereyra@gmail.com>
2024-06-15 11:54:23 -0300

    Corrección de create conecction
    funcion de crear la conexion se ejecuta adentro de las funciones que usas para los python operator sin necesidad de pasarselas como argumento

--- dags/Etl_Crypto.py
+++ dags/Etl_Crypto.py
@@ -1,3 +1,4 @@
+'''
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -163,6 +164,141 @@ def main():
             conn.close()
         else:
             print("No se pudo establecer conexión a Redshift.")
+    else:
+        print("No se pudieron obtener datos de criptomonedas.")
+
+if __name__ == "__main__":
+    main()
+'''
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import requests
+import pandas as pd
+import psycopg2
+from datetime import datetime
+from airflow.models import Variable
+
+# Variables
+Pass_Email = Variable.get("gmail_secret")
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+sender_email = 'machimizado@gmail.com'
+password = Pass_Email
+
+# Función para enviar email
+def send_email(subject, body_text):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = sender_email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body_text, 'plain'))
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.send_message(msg)
+        print('El email fue enviado correctamente.')
+
+    except Exception as exception:
+        print(exception)
+        print('El email no se pudo enviar.')
+
+def get_crypto_data():
+    base_url = "https://api.coincap.io/v2"
+    endpoint = "/assets"
+    headers = {"Accept-Encoding": "gzip, deflate"}
+    url = base_url + endpoint
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        df = pd.DataFrame(data['data'])
+        fecha_consulta = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        df['fecha_consulta'] = fecha_consulta
+        df.drop(['id'], axis=1, inplace=True)
+        symbol_column = df.pop('symbol')
+        df.insert(0, 'id', symbol_column)
+        return df
+    else:
+        print("Error al realizar la solicitud:", response.status_code)
+        return None
+
+def create_redshift_connection():
+    url = "data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com"
+    data_base = "data-engineer-database"
+    user = "matiaspereyra_coderhouse"
+    pwd = Variable.get("redshift_secret")
+    try:
+        conn = psycopg2.connect(
+            host=url,
+            dbname=data_base,
+            user=user,
+            password=pwd,
+            port='5439'
+        )
+        print("Conectado a Redshift con éxito!")
+        return conn
+    except Exception as e:
+        print("No es posible conectar a Redshift")
+        print(e)
+        return None
+
+def create_crypto_table():
+    conn = create_redshift_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS matiaspereyra_coderhouse.crypto
+            (
+                id VARCHAR(50) primary key,
+                rank INTEGER,
+                name VARCHAR(255),
+                supply DECIMAL,
+                maxSupply DECIMAL,
+                marketCapUsd DECIMAL,
+                volumeUsd24Hr DECIMAL,
+                priceUsd DECIMAL,
+                changePercent24Hr DECIMAL,
+                vwap24Hr DECIMAL,
+                explorer VARCHAR(255),
+                fecha_consulta TIMESTAMP,
+                fecha_insercion TIMESTAMP
+            )
+        """)
+        conn.commit()
+        print("Tabla de criptomonedas creada exitosamente en Redshift.")
+    except Exception as e:
+        print("Error al crear la tabla de criptomonedas en Redshift:", e)
+
+def insert_data_into_redshift(df):
+    conn = create_redshift_connection()
+    if conn is not None:
+        try:
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM crypto")
+            conn.commit()
+
+            for index, row in df.iterrows():
+                cursor.execute("INSERT INTO crypto (id, rank, name, supply, maxSupply, marketCapUsd, volumeUsd24Hr, priceUsd, changePercent24Hr, vwap24Hr, explorer, fecha_consulta, fecha_insercion) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                               (row['id'], row['rank'], row['name'], row['supply'], row['maxSupply'], row['marketCapUsd'], row['volumeUsd24Hr'], row['priceUsd'], row['changePercent24Hr'], row['vwap24Hr'], row['explorer'], row['fecha_consulta'], datetime.now()))
+            conn.commit()
+            print("Registros insertados correctamente en Redshift.")
+        except Exception as e:
+            print("Error al insertar registros en Redshift:", e)
+            conn.rollback()
+        finally:
+            cursor.close()
+            conn.close()
+    else:
+        print("No se pudo establecer conexión a Redshift.")
+
+def main():
+    df = get_crypto_data()
+    if df is not None:
+        create_crypto_table()
+        insert_data_into_redshift(df)
     else:
         print("No se pudieron obtener datos de criptomonedas.")
 
--- dags/dags_etl.py
+++ dags/dags_etl.py
@@ -1,4 +1,4 @@
-import pandas as pd
+'''import pandas as pd
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
@@ -60,3 +60,63 @@ send_email_task = PythonOperator(
 
 # Definir el flujo de tareas
 get_crypto_data_task >> create_redshift_connection_task >> create_crypto_table_task >> insert_data_into_redshift_task >> send_email_task
+'''
+import pandas as pd
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+from Etl_Crypto import get_crypto_data, create_redshift_connection, create_crypto_table, insert_data_into_redshift, send_email
+
+# Definir los argumentos predeterminados del DAG
+default_args = {
+    'owner': 'matiaspereyra',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 5, 19),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Definir el DAG
+dag = DAG('etl_crypto', default_args=default_args, schedule_interval='@daily')
+
+# Tareas
+get_crypto_data_task = PythonOperator(
+    task_id='get_crypto_data',
+    python_callable=get_crypto_data,
+    dag=dag,
+    do_xcom_push=False  
+)
+
+create_redshift_connection_task = PythonOperator(
+    task_id='create_redshift_connection',
+    python_callable=create_redshift_connection,
+    dag=dag,
+    do_xcom_push=False  
+)
+
+create_crypto_table_task = PythonOperator(
+    task_id='create_crypto_table',
+    python_callable=create_crypto_table,
+    dag=dag,
+)
+
+insert_data_into_redshift_task = PythonOperator(
+    task_id='insert_data_into_redshift',
+    python_callable=insert_data_into_redshift,
+    op_args=[get_crypto_data()],  
+    dag=dag,
+)
+
+send_email_task = PythonOperator(
+    task_id='send_email',
+    python_callable=send_email,
+    op_kwargs={'subject': 'Carga de datos', 'body_text': 'Los datos fueron cargados a la base de datos exitosamente.'},
+    dag=dag,
+)
+
+# Definir el flujo de tareas
+get_crypto_data_task >> create_redshift_connection_task >> create_crypto_table_task >> insert_data_into_redshift_task >> send_email_task
+
